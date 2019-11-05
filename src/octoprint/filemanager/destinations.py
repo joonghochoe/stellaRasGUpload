e2654e6439e2806d1ef0bc37d47f6c6e26d4ee4f
Joongho Choe <joongho.choe@gmail.com>
2019-11-05 10:58:46 +0900

    Revert "initial commit"

--- src/octoprint/filemanager/destinations.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from __future__ import absolute_import, division, print_function
-__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
-
-class FileDestinations(object):
-
-	SDCARD = "sdcard"
-	LOCAL = "local"
--- tests/plugins/pi_support/fakes/fake_vcgencmd.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python2
-print("throttled=0x50005")
